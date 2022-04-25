c446ab8bd1a486602f644be335ec428b5384b2c1
Fahid <54591561+fahid99@users.noreply.github.com>
2022-04-25 18:10:21 -0400

    Update onetimepad.py

--- onetimepad.py
+++ onetimepad.py
@@ -24,31 +24,33 @@ def encrypt(cleartext, key):
 	for i in keyASCII:
 		keyBin.append(bin(i))
 
-	print("----------This is the binary representation of the cleartext and private key, respectively----------")
+	print("---------- This is the binary representation of the cleartext and private key, respectively ----------")
 	print(cleartextBin, keyBin)
 
 	# -------------------------- XOR'ing the cleartext and private key to generate the ciphertext --------------------------
 
 	if len(cleartext) == len(key):
-		print("----------This is the encrypted text in binary----------")
+		print("---------- This is binary representation of the encrypted text ----------")
 		for i in range(len(cleartextBin)):
 			ciphertext = int(cleartextBin[i], 2) ^ int(keyBin[i], 2)
 			print(bin(ciphertext), end = " ")
 	else:
 		print("The text cannot be encrypted securely since the text and key are not of equal bit length")
 
-# encrypt("cat", "hat")
+# EXAMPLE: encrypt("cat", "hat")
 
 # -------------------------- XOR'ing the ciphertext and private key to decrypt the ciphertext --------------------------
 
 def decrypt(ciphertext, key):
 
-	cleartext = []
+	ciphertext = []
+	key = []
 	
-	for i in range(ciphertext):
+	cleartext = []
+	for i in range(len(ciphertext)):
 		cleartext = ciphertext ^ key
 		cleartext.append(ord(i))
-	print("\n----------This is the decrypted text ----------")
-	print(bin(cleartext))
+	print("\n---------- This is the decrypted text ----------")
+	print(cleartext)
 
-# decrypt(0b10110000000000000000,0b11010000110000101110100)
+# EXAMPLE: decrypt([0b1011000, 0b0000000, 0b0000000],[0b1101000, 0b1100001, 0b1110100])
